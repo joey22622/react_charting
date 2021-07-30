957df320db25425de7f7c85aa64c0a8cb355147d
joey2262 <connect@josephdechant.com>
2021-07-29 22:19:18 -0600

    made websocket connection wss

--- src/components/Data.tsx
+++ src/components/Data.tsx
@@ -20,7 +20,7 @@ const httpLink = new HttpLink({
 });
 
 const wsLink = new WebSocketLink({
-    uri: 'ws://react.eogresources.com/graphql',
+    uri: 'wss://react.eogresources.com/graphql',
     options: {
         reconnect: true
     }
